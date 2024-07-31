50869219fb61bafcd37ac0b24921f1261c69f16b
tommyc2 <20101841@mail.wit.ie>
2024-07-31 22:11:39 +0100

    Add file opening functionality (no audio playback yet)

--- app/src/main/java/com/example/tunevault/MainActivity.java
+++ app/src/main/java/com/example/tunevault/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.tunevault;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
+import android.view.View;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.Nullable;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +24,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    public void addSong(View view) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("audio/*");
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            if (intent != null) {
+                Uri uri = intent.getData();
+
+                System.out.println("File Path - " + uri.toString());
+            }
+        }
     }
 
 
